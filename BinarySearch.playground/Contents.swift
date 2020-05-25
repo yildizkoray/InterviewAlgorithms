acc0b4e59c172f1fb83ebab9368a81204ff56b31
yildizkoray <koray.yildizz@gmail.com>
2020-05-26 00:45:35 +0300

    BinarySearch

---
+++ BinarySearch.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+let array = [1, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+func binarySearch(for array: [Int], key: Int) -> Int {
+    
+    guard array.count != 0 else { return -1 }
+    
+    let firstIndex = 0
+    let lastIndex = array.count - 1
+    let middleIndex = lastIndex / 2
+    
+    if key < array[firstIndex] || key > array[lastIndex] {
+        return -1
+    }
+    
+    if key > array[middleIndex] {
+        let slice = Array(array[middleIndex + 1 ... lastIndex])
+        return binarySearch(for: slice, key: key)
+    }
+    
+    if key < array[middleIndex] {
+        let slice = Array(array[firstIndex ... middleIndex - 1])
+        return binarySearch(for: slice, key: key)
+    }
+    
+    if key == array[middleIndex] {
+        return key
+    }
+    
+    return -1
+}
+
+binarySearch(for: array, key: 2)
+
+
