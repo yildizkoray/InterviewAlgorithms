71a9934cfa6d68eded605f4c444df2f57fbba2cb
yildizkoray <koray.yildizz@gmail.com>
2020-05-26 00:04:56 +0300

    Palindrome

---
+++ Palindrome.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+func isPalindrome(text: String) -> Bool {
+    
+    let arrayOfText = Array(text)
+    
+    for index in 0 ..< text.count / 2 {
+        
+        if arrayOfText[index] == arrayOfText[arrayOfText.count - 1] {
+            return true
+        }
+    }
+    
+    return false
+}
+
+isPalindrome(text: "abbccbba")
+isPalindrome(text: "Koray")
