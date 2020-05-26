299a34699bfaf4c5f2302aca1d3ada1eaa1cfefe
yildizkoray <koray.yildizz@gmail.com>
2020-05-26 10:19:15 +0300

    SwapTwoNumber

---
+++ SwapTwoNumbers.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var first = 27
+var second = 10
+
+print("First: \(first)")
+print("Second: \(second)")
+
+first = first + second
+first = first - second
+second = first - second
+
+print("First: \(first)")
+print("Second: \(second)")
+
